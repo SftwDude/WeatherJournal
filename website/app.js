4dc366db0533dc29565ab89f4c2871fd8ec5405b
Mike Berghoff <mberghoff@amt-corp.com>
2020-05-15 11:30:18 -0400

    add getWeather to fetch from openweather.com api

--- website/app.js
+++ website/app.js
@@ -1,5 +1,29 @@
 /* Global Variables */
+let baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+let apiKey = '&appid=77eb9eb366ce5b4d72d4edb6560b06f6';
+let units = '&units=imperial';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+document.getElementById('generate').addEventListener('click', performAction);
+
+function performAction(e){
+const newZip =  document.getElementById('zip').value;
+getWeather(baseURL,newZip, units, apiKey)
+
+}
+const getWeather = async (baseURL, zip, key, units)=>{
+
+  const res = await fetch(baseURL+zip+units+key)
+  try {
+
+    const data = await res.json();
+    console.log(data)
+    return data;
+  }  catch(error) {
+    console.log("error", error);
+    // appropriately handle the error
+  }
+}
